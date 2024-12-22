bb33025988640e24dece99aff1261846cb81ec23
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2024-12-22 19:17:43 +0600

    work on the user registration system

--- app/Models/RegistrationModel.php
+++ app/Models/RegistrationModel.php
@@ -6,12 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistrationModel extends Model
 {
-    protected $table='registration';
+    protected $table = 'registration';
 
-    protected $primarykey='id';
-
-    public $incrementing ='true';
-    protected $keyType='int';
-
-    public $timestamps=false;
+    protected $fillable = ['firstname', 'lastname', 'city', 'username', 'password', 'gender'];
 }
