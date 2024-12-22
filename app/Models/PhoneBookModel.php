ed8171d7603382eef32367a7c50b608449c2c5a9
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2024-12-22 13:30:46 +0600

    work on the phonebook model

--- app/Models/PhoneBookModel.php
+++ app/Models/PhoneBookModel.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class PhoneBookModel extends Model
 {
+    protected $table='phone_book_details';
 
+    protected $primarykey='id';
+
+    public $incrementing ='true';
+    protected $keyType='int';
+
+    public $timestamps=false;
 }
