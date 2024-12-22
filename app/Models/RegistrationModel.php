ed5579aca564061cbcfd7b04a2323d954772aebd
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2024-12-22 13:33:06 +0600

    work on the Registrstion Model

--- app/Models/RegistrationModel.php
+++ app/Models/RegistrationModel.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegistrationModel extends Model
 {
+    protected $table='registration';
 
+    protected $primarykey='id';
+
+    public $incrementing ='true';
+    protected $keyType='int';
+
+    public $timestamps=false;
 }
