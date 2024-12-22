c928c146cb6f6d04159dc382b6bc627abbbdba68
Ashik-PUST-ICE <ashik.200607@s.pust.ac.bd>
2024-12-22 13:31:55 +0600

    work on the Accesstoken model

--- app/Models/AccessTokenModel.php
+++ app/Models/AccessTokenModel.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccessTokenModel extends Model
 {
+    protected $table='access_token';
+
+    protected $primarykey='id';
+
+    public $incrementing ='true';
+    protected $keyType='int';
+
+    public $timestamps=false;
 
 }
